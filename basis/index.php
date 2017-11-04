3b1a498759d7cb7b4e33e11212eef35b899c19f3
sgehin <sfa.gehin@gmail.com>
2017-11-04 11:16:27 +0100

    bord

--- basis/index.php
+++ basis/index.php
@@ -11,22 +11,46 @@
             table , tr, td {
                 border:1px solid gray;
             }
-                      
+            button{
+                width: 20;
+                height: 20;
+            }          
            
         </style>
         
         
     </head>
     <body>
+   
+    
+    
+    <tabel>
+        <tr>
                     
        <?php
+       // bord opmaken
+       for($row=0;$row<4;$row++){
+            $idlabel=array(1,2,3,4,5,6,7,8);
+            for($x=0;$x< count($idlabel);$x++){
+                echo "<td><button type='button' onclik=myfunctie() id=$idlabel[$x]>-</button></td>";
+                echo '<td><button type="button" onclik="myfunctie() id="b7">X</button></td>';                
+            }
+            echo "<br>";
+       }    
+        ?>
+        </tr>
+    </tabel> 
+    <script>
+        
+            function myfunctie(){
+                alert("test");
+                document.getElementById('b7').innerhtml='test';
+                }
+                       
+            }
+                    
+    </script>
+            
        
-       $idlabel=array(1,2,3,4,5,6,7,8);
-       for($x=0;$x< count($idlabel);$x++){
-             echo "<table><tr><td>$idlabel[$x]</td></tr></table>";
-       }
-       
-       
-       ?>
     </body>
 </html>
