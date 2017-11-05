e80e75a6366f54940b2b3380e8d5364b5e44444b
sgehin <sfa.gehin@gmail.com>
2017-11-05 12:38:32 +0100

    bord met id tag per vak

--- basis/index.php
+++ basis/index.php
@@ -1,7 +1,8 @@
+<!DOCTYPE html>
 <html>
     <head>
         <link rel="stylesheet" type="text/css" href="myStyle.css">    
-    <script src="myJavascript.js"></script> 
+    <script src="battlescript.js"></script> 
         <script>
                                            
           
@@ -11,46 +12,22 @@
             table , tr, td {
                 border:1px solid gray;
             }
-            button{
-                width: 20;
-                height: 20;
-            }          
+                      
            
         </style>
         
         
     </head>
     <body>
-   
-    
-    
-    <tabel>
-        <tr>
                     
        <?php
-       // bord opmaken
-       for($row=0;$row<4;$row++){
-            $idlabel=array(1,2,3,4,5,6,7,8);
-            for($x=0;$x< count($idlabel);$x++){
-                echo "<td><button type='button' onclik=myfunctie() id=$idlabel[$x]>-</button></td>";
-                echo '<td><button type="button" onclik="myfunctie() id="b7">X</button></td>';                
-            }
-            echo "<br>";
-       }    
-        ?>
-        </tr>
-    </tabel> 
-    <script>
-        
-            function myfunctie(){
-                alert("test");
-                document.getElementById('b7').innerhtml='test';
-                }
-                       
-            }
-                    
-    </script>
-            
+       // waarde van variable echoen tussen htmltags
+       $idlabel=array(1,2,3,4,5,6,7,8);
+       for($x=0;$x< count($idlabel);$x++){
+             echo "<table><tr><td>$idlabel[$x]</td></tr></table>";
+       }
        
+       
+       ?>
     </body>
 </html>
--- zeeslag/index.php
+++ zeeslag/index.php
@@ -1,33 +1,104 @@
 <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
     <head>
-        <link rel="stylesheet" type="text/css" href="myStyle.css">    
-    <script src="myJavascript.js"></script> 
+                                                                    <!-- voeg ico aan tablad -->
+        <link rel="shortcut icon" href="img/icon-battleship-180x180_3.png" />
+        <title>Battleship</title>                                   <!-- voeg tilee aan tablad -->
+        <link rel="stylesheet" type="text/css" href="myStyle.css">  <!-- koppeling tussen html en css -->   
+    <script src="battlescript.js"></script>                         <!-- koppeleing tussen html en js --> 
+   
         <script>
-                                           
-          
-            
+                   
          </script>
         <style>
-            table , tr, td {
-                border:1px solid gray;
-            }
-                      
-           
+               
         </style>
         
         
     </head>
     <body>
-                    
-       <?php
-       // waarde van variable echoen tussen htmltags
-       $idlabel=array(1,2,3,4,5,6,7,8);
-       for($x=0;$x< count($idlabel);$x++){
-             echo "<table><tr><td>$idlabel[$x]</td></tr></table>";
-       }
-       
-       
-       ?>
+        <table>
+            <tr>
+                <?php
+                    $kol=0;
+                    for($tel=0;$tel<10;$tel++){
+                    echo "<td id=$kol$tel></td>";
+                    }
+                ?>
+            </tr>
+            <tr>
+                <?php
+                    $kol=1;
+                    for($tel=0;$tel<10;$tel++){
+                    echo "<td id=$kol$tel></td>";
+                    }
+                ?>
+            </tr>
+            <tr>
+                <?php
+                    $kol=2;
+                    for($tel=0;$tel<10;$tel++){
+                    echo "<td id=$kol$tel></td>";
+                    }
+                ?>
+            </tr>
+            <tr>
+                <?php
+                    $kol=3;
+                    for($tel=0;$tel<10;$tel++){
+                    echo "<td id=$kol$tel></td>";
+                    }
+                ?>
+            </tr>
+            <tr>
+                <?php
+                    $kol=4;
+                    for($tel=0;$tel<10;$tel++){
+                    echo "<td id=$kol$tel></td>";
+                    }
+                ?>
+            </tr>
+            <tr>
+                <?php
+                    $kol=5;
+                    for($tel=0;$tel<10;$tel++){
+                    echo "<td id=$kol$tel></td>";
+                    }
+                ?>
+            </tr>
+            <tr>
+                <?php
+                    $kol=6;
+                    for($tel=0;$tel<10;$tel++){
+                    echo "<td id=$kol$tel></td>";
+                    }
+                ?>
+            </tr>
+            <tr>
+                <?php
+                    $kol=7;
+                    for($tel=0;$tel<10;$tel++){
+                    echo "<td id=$kol$tel></td>";
+                    }
+                ?>
+            </tr>
+            <tr>
+                <?php
+                    $kol=8;
+                    for($tel=0;$tel<10;$tel++){
+                    echo "<td id=$kol$tel></td>";
+                    }
+                ?>
+            </tr>
+            <tr>
+                <?php
+                    $kol=9;
+                    for($tel=0;$tel<10;$tel++){
+                    echo "<td id=$kol$tel></td>";
+                    }
+                ?>
+            </tr>
+        </table>
     </body>
 </html>
